4f82cb5b8bd972eae7e83ae6f74b129cebae0174
Buzin Igor <i.b@msulab.ru>
2020-09-01 19:53:09 +0300

    first part

---
+++ src/generator.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Gendiff\generator;
+
+use Funct\Collection;
+
+function generateDiff($filePath1, $filePath2){
+    $rawDataBefore = file_get_contents($filePath1);
+    $rawDataAfter = file_get_contents($filePath2);
+    $parsedDataBefore = json_decode($rawDataBefore, true);
+    $parsedDataAfter = json_decode($rawDataAfter, true);
+
+    $keys = Collection\union(array_keys($parsedDataBefore), array_keys($parsedDataAfter));
+    $ast = array_reduce($keys, function ($acc, $key) use ($parsedDataBefore, $parsedDataAfter) {
+        $acc[] = getTypes($key, $parsedDataBefore, $parsedDataAfter);
+        return $acc;
+    });
+    return $ast;
+}
+
+function getTypes($key, $before, $after)
+{
+    
+    if (!array_key_exists($key, $before)) {
+        return ['type' => 'added', 'key' => $key, 'value' => $after[$key]];
+    }
+    if (!array_key_exists($key, $after)) {
+        return ['type' => 'deleted', 'key' => $key, 'value' => $before[$key]];
+    }
+    if (is_array($before[$key]) && is_array($after[$key])) {
+        return ['type' => 'parent', 'name' => $key, 'children' => generateDiff($before[$key], $after[$key])];
+    }
+    if ($before[$key] === $after[$key]) {
+        return ['type' => 'unchanged', 'key' => $key, 'value' => $before[$key]];
+    }
+    if ($before[$key] !== $after[$key]) {
+        return ['type' => 'changed', 'key' => $key, 'oldValue' => $before[$key], 'newValue' => $after[$key]];
+    }
+}
---
+++ src/runner.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Gendiff\runner;
+
+use Docopt;
+use function Gendiff\generator\generateDiff;
+
+function run()
+{
+    $doc = <<<DOC
+	Generate diff
+	Usage:
+	  gendiff (-h|--help)
+	  gendiff [--format <fmt>] <firstFile> <secondFile>
+	Options:
+	  -h --help                     Show this screen
+	  --format <fmt>                Report format [default: pretty]
+	DOC;
+
+    $args = Docopt::handle($doc);
+    $filePath1 = $args['<firstFile>'];
+    $filePath2 = $args['<secondFile>'];
+    $format = $args['--format'];
+        
+    $result = generateDiff($filePath1, $filePath2);
+
+    $resultToPrint = array_reduce($result, function ($acc, $item) {
+        switch ($item['type']) {
+            case 'unchanged':
+                $acc[] = ['sort' => $item['key'], 'result' => "  {$item['key']}: {$item['value']}"];
+            break;
+            case 'changed':
+                $acc[] = ['sort' => $item['key'], 'result' => "- {$item['key']}: {$item['oldValue']}"];
+                $acc[] = ['sort' => $item['key'], 'result' => "+ {$item['key']}: {$item['newValue']}"];
+            break;
+            case 'deleted':
+                $acc[] = ['sort' => $item['key'], 'result' => "- {$item['key']}: {$item['value']}"];
+            break;
+            case 'added':
+                $acc[] = ['sort' => $item['key'], 'result' => "+ {$item['key']}: {$item['value']}"];
+            break;
+        }
+        return $acc;
+    }, []);
+    usort($resultToPrint, function ($a, $b) {
+        return strcmp($a["sort"], $b["sort"]);
+    });
+    foreach ($resultToPrint as $result) {
+        echo "{$result['result']} \n";
+    };
+}
