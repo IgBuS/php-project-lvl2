f0262b45a5f22f24e636a5d9a6dd71e7f7eaaf89
Buzin Igor <i.b@msulab.ru>
2020-09-14 22:48:08 +0300

    tests refactioring

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -8,50 +8,56 @@ use function Gendiff\Generator\generateDiff;
 
 class UserTest extends TestCase
 {
-    public function testJson(): void
-    {
-        $pathToFirstFileToCompare = __DIR__ . "/fixtures/flatBefore.json";
-        $pathToSecondFileToCompare = __DIR__ . "/fixtures/flatAfter.json";
+    /**
+    * @dataProvider testProvider
+    */
 
-        $correctAnswer = file_get_contents(__DIR__ . "/fixtures/expected/expFlatDiff");
-        $this->assertEquals($correctAnswer, generateDiff($pathToFirstFileToCompare, $pathToSecondFileToCompare));
+    public function testDiff($pathToFirstFileToCompare, $pathToSecondFileToCompare, $pathToFileWithCorrectAnswer, $format): void
+    {
+
+        $pathToFirstFileToCompare = $this->getFilePath($pathToFirstFileToCompare);
+        $pathToSecondFileToCompare = $this->getFilePath($pathToSecondFileToCompare);
+
+        $correctAnswer = file_get_contents($this->getFilePath($pathToFileWithCorrectAnswer, 'answer'));
+        $this->assertEquals($correctAnswer, generateDiff($pathToFirstFileToCompare, $pathToSecondFileToCompare, $format));
+    }
+    
+    public function testProvider()
+    {
+        return [
+            "prettyJsonDiff" => [
+                "flatBefore.json",
+                "flatAfter.json",
+                "expFlatDiff",
+                "basic"
+            ],
+            "prettyYamlDiff" => [
+                "flatBefore.yaml",
+                "flatAfter.yaml",
+                "expFlatDiff",
+                "basic"
+            ],
+            "plainRecursiveJson" => [
+                "recursiveBefore.json",
+                "recursiveAfter.json",
+                "expRecursiveDiffPlain",
+                "plain"
+            ],
+            "jsonRecursiveJson" => [
+                "recursiveBefore.json",
+                "recursiveAfter.json",
+                "expRecursiveDiffJson",
+                "json"
+            ]
+        ];
     }
 
-    public function testYaml(): void
+    public function getFilePath($fileName, $fileType = 'example')
     {
-        $pathToFirstFileToCompare = __DIR__ . "/fixtures/flatBefore.yaml";
-        $pathToSecondFileToCompare = __DIR__ . "/fixtures/flatAfter.yaml";
-
-        $correctAnswer = file_get_contents(__DIR__ . "/fixtures/expected/expFlatDiff");
-        $this->assertEquals($correctAnswer, generateDiff($pathToFirstFileToCompare, $pathToSecondFileToCompare));
-    }
-
-    public function testRecursiveFiles(): void
-    {
-        $pathToFirstFileToCompare = __DIR__ . "/fixtures/recursiveBefore.json";
-        $pathToSecondFileToCompare = __DIR__ . "/fixtures/recursiveAfter.json";
-
-        $correctAnswer = file_get_contents(__DIR__ . "/fixtures/expected/expRecursiveDiff");
-        $this->assertEquals($correctAnswer, generateDiff($pathToFirstFileToCompare, $pathToSecondFileToCompare));
-    }
-
-    public function testRecursiveFilesWithPlainOutput(): void
-    {
-        $pathToFirstFileToCompare = __DIR__ . "/fixtures/recursiveBefore.json";
-        $pathToSecondFileToCompare = __DIR__ . "/fixtures/recursiveAfter.json";
-
-        $correctAnswer = file_get_contents(__DIR__ . "/fixtures/expected/expRecursiveDiffPlain");
-        $resultToCheck = generateDiff($pathToFirstFileToCompare, $pathToSecondFileToCompare, 'plain');
-        $this->assertEquals($correctAnswer, $resultToCheck);
-    }
-
-    public function testRecursiveFilesWithJasonOutput(): void
-    {
-        $pathToFirstFileToCompare = __DIR__ . "/fixtures/recursiveBefore.json";
-        $pathToSecondFileToCompare = __DIR__ . "/fixtures/recursiveAfter.json";
-
-        $correctAnswer = file_get_contents(__DIR__ . "/fixtures/expected/expRecursiveDiffJson");
-        $resultToCheck = generateDiff($pathToFirstFileToCompare, $pathToSecondFileToCompare, 'json');
-        $this->assertEquals($correctAnswer, $resultToCheck);
+        if ($fileType == 'example') {
+            return __DIR__ . "/fixtures/{$fileName}";
+        } else {
+            return __DIR__ . "/fixtures/expected/{$fileName}";
+        }
     }
 }
